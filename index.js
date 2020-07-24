eeb9aa5e9a06bdc60a71ab36494b28d081f04a1a
kislev-owo <kislev_083@hotmail.com>
2020-07-24 04:07:50 +0000

    cambios agregados al for y el tamaño de los numeros y los simbolos de la carta

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ window.onload = () => {
     carta.style.background = "white"
     carta.style.width = "15rem";
     carta.style.height = "20rem";
-    carta.style.position = "relative";
-
+    carta.style.position = "relative";  
+    carta.style.fontSize = "50px";  
     //PINTA arriba y abajo 
     let top = document.createElement("div");
     top.style.position = "absolute";
@@ -17,24 +17,22 @@ window.onload = () => {
     top.style.width = "1rem";
     top.style.left = "50%";
     top.style.top = "50%";
-    top.style.transform = "translate(-600%, -800%)";
-
+    top.style.transform = "translate(-600%, -800%)"; 
     let bottom = document.createElement("div");
     bottom.style.position = "absolute";
     bottom.style.height = "1rem";
     bottom.style.width = "1rem";
     bottom.style.left = "50%";
     bottom.style.top = "50%";
-    bottom.style.transform = "translate(500%, 700%)";
-
-   //numero que esta en el medio:
+    bottom.style.transform = "translate(500%, 700%)"; 
+    //numero del medio:
     carta.innerHTML = `    
         <div class="w-100 h-100 justify-content-center d-flex align-items-center">
         </div>
     `;
     //Pinta de arriba
     top.innerHTML = `    
-       <div class="w-50 h-100 justify-content-center d-flex align-items-center">
+       <div class="w-100 h-100 justify-content-center d-flex align-items-center">
         </div>
     `;
     //Pinta de abajo
@@ -43,30 +41,24 @@ window.onload = () => {
         </div>
     `;
     // Arreglos 
-    let numbers = ["A", "2", "3", "4","5","6","7","8","9","10","J","Q","K"];
-    let simbolos = ["♠","♣","♥","♦"];
-
+    let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let simbolos = ["♠", "♣", "♥", "♦"];
+    //Aqui estoy generando los random no dentro de los for y estos mismos los uso dentro de los for ya que la imagen va a ser la misma 
+    let randomIndex = Math.floor(Math.random() * numbers.length);
+    let randomIndex2 = Math.floor(Math.random() * simbolos.length);
     for (child of carta.children) {
-        let randomIndex = Math.floor(Math.random() * numbers.length);
         child.style.height = "10rem";
         child.classList.add(`${numbers[randomIndex]}`);
         child.innerHTML = `<p class="text-center">${numbers[randomIndex]}</p>`;
     } 
-    
     for (child of top.children) {
-    let randomIndex2 = Math.floor(Math.random() * simbolos.length);
         child.classList.add(`${simbolos[randomIndex2]}`);
         child.innerHTML = `${simbolos[randomIndex2]}`;
     }
     for (child of bottom.children) {
-    let randomIndex3 = Math.floor(Math.random() * simbolos.length);
-        child.classList.add(`${simbolos[randomIndex3]}`);
-        child.innerHTML = `${simbolos[randomIndex3]}`;
-    }
-
-
-    
-    
+        child.classList.add(`${simbolos[randomIndex2]}`);
+        child.innerHTML = `${simbolos[randomIndex2]}`;
+    } 
     carta.appendChild(bottom);
     carta.appendChild(top);
     container.appendChild(carta);
